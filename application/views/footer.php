7f6c5bc04800454dc57b332c9313d22d82e32e0e
nomena <vahatra.nomena@yahoo.com>
2023-07-10 23:21:54 +0300

    modif

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,19 @@
+
+<style>
+.footer {
+      background-color: #f8f9fa; /* Footer background color */
+      height: 20px; /* Adjust the height as per your requirements */
+      line-height: 50px;
+
+      color: #333; /* Adjusted contrast color */
+      padding: 20px;
+
+      text-align: center;
+    }
+</style>
+
 <footer class="bg-dark text-light text-center py-3">
   <div class="container">
-    <span>&copy; <?php echo date('Y'); ?> App Nutrition</span>
+    <span>&copy; <?php echo date('Y'); ?>- App Nutrition</span>
   </div>
 </footer>
