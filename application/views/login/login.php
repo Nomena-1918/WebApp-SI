f46396b67a8885783a242987dfbc56a6d07a1c9a
ghostKely <120150720+ghostKely@users.noreply.github.com>
2023-07-11 19:19:15 +0300

    go

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -10,21 +10,21 @@
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
 
 	<!-- CSS here -->
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>">
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/owl.carousel.min.css');?>">
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/slicknav.css');?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/flaticon.css');?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/progressbar_barfiller.css');?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/gijgo.css');?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/animate.min.css');?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/animated-headline.css');?>">
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/magnific-popup.css');?>">
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/fontawesome-all.min.css');?>">
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/themify-icons.css');?>">
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/slick.css');?>">
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/nice-select.css');?>">
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css');?> ">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/_modification_style.css');?> ">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plat/css/bootstrap.min.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plat/css/owl.carousel.min.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plat/css/slicknav.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/plat/css/flaticon.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/plat/css/progressbar_barfiller.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/plat/css/gijgo.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/plat/css/animate.min.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/plat/css/animated-headline.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plat/css/magnific-popup.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plat/css/fontawesome-all.min.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plat/css/themify-icons.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plat/css/slick.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plat/css/nice-select.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plat/css/style.css');?> ">
+    <link rel="stylesheet" href="<?php echo base_url('assets/plat/css/_modification_style.css');?> ">
 
 </head>
 <body>
@@ -78,45 +78,45 @@
 </main>
 
 
-<script src="<?php echo base_url('assets/js/vendor/modernizr-3.5.0.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/plat/js/vendor/modernizr-3.5.0.min.js'); ?>"></script>
     <!-- Jquery, Popper, Bootstrap -->
-    <script src="<?php echo base_url('assets/js/vendor/jquery-1.12.4.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/popper.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/vendor/jquery-1.12.4.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/popper.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/bootstrap.min.js'); ?>"></script>
     <!-- Jquery Mobile Menu -->
-    <script src="<?php echo base_url('assets/js/jquery.slicknav.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.slicknav.min.js'); ?>"></script>
 
     <!-- Video bg -->
-    <script src="<?php echo base_url('assets/js/jquery.vide.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.vide.js'); ?>"></script>
 
     <!-- Jquery Slick , Owl-Carousel Plugins -->
-    <script src="<?php echo base_url('assets/js/owl.carousel.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/slick.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/owl.carousel.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/slick.min.js'); ?>"></script>
     <!-- One Page, Animated-HeadLin -->
-    <script src="<?php echo base_url('assets/js/wow.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/animated.headline.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/jquery.magnific-popup.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/wow.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/animated.headline.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.magnific-popup.js'); ?>"></script>
 
     <!-- Date Picker -->
-    <script src="<?php echo base_url('assets/js/gijgo.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/gijgo.min.js'); ?>"></script>
     <!-- Nice-select, sticky -->
-    <script src="<?php echo base_url('assets/js/jquery.nice-select.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/jquery.sticky.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.nice-select.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.sticky.js'); ?>"></script>
     <!-- Progress -->
-    <script src="<?php echo base_url('assets/js/jquery.barfiller.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.barfiller.js'); ?>"></script>
     
     <!-- counter , waypoint,Hover Direction -->
-    <script src="<?php echo base_url('assets/js/jquery.counterup.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/waypoints.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/jquery.countdown.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/hover-direction-snake.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.counterup.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/waypoints.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.countdown.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/hover-direction-snake.min.js'); ?>"></script>
 
     <!-- contact js -->
-    <script src="<?php echo base_url('assets/js/contact.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/jquery.form.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/jquery.validate.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/mail-script.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/jquery.ajaxchimp.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/contact.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.form.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.validate.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/mail-script.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/plat/js/jquery.ajaxchimp.min.js'); ?>"></script>
     
     <!-- Jquery Plugins, main Jquery -->	
     <script src="<?php echo base_url('assets/js/plugins.js'); ?>"></script>
